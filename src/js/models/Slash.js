63c5cc67aaaf086d9693923af623a3759e9378bb
Luis Boto <43352462+LuisBoto@users.noreply.github.com>
2023-05-01 12:39:47 +0200

    Slash rotation now calculated by frame

--- src/js/models/Slash.js
+++ src/js/models/Slash.js
@@ -12,17 +12,17 @@ class Slash extends Model {
     initiate() {
         this.frames = 3;
         this.show = false;
-        this.rotation = Math.floor(Math.random()*360) - 45;
     }
 
     draw() {
         if (this.show && this.frames >= 0) {
             context.fillStyle = "black";
             context.fillRect(0, 0, canvasWidth, canvasHeight);
+            let rotation = Math.floor(Math.random()*360);
 
             context.save();
             context.translate(canvasWidth*0.5, canvasHeight*0.65);
-            context.rotate(this.rotation * (Math.PI / 180));
+            context.rotate(rotation * (Math.PI / 180));
             super.drawResize(this.width*6, this.height*6);
             context.restore();
             this.frames -= 1;
