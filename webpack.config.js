85c3441590ea7a99a9b610db3181f31538fdf614
luisboto <43352462+LuisBoto@users.noreply.github.com>
2023-05-19 11:45:15 +0200

    Added css loader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,10 @@ module.exports = {
        test: /\.(png|svg|jpg|jpeg|gif|mp3)$/i,
        type: 'asset/resource',
      },
+     {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+     },
     ],
   },
 };
