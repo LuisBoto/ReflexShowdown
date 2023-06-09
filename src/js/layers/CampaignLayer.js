78f1d18c14fcb5d872a23639cf7366ae2e67d1be
luisboto <43352462+LuisBoto@users.noreply.github.com>
2023-06-09 19:40:45 +0200

    Added match rush difficulty progression

--- src/js/layers/CampaignLayer.js
+++ src/js/layers/CampaignLayer.js
@@ -1,5 +1,5 @@
 import { GameLayer } from "./GameLayer.js";
-import { setLayer, canvasWidth, getCanvasProportionSize } from "../../Main.js";
+import { canvasWidth, getCanvasProportionSize } from "../../Main.js";
 import { StrokedText } from "../models/StrokedText.js";
 
 
@@ -13,11 +13,12 @@ class CampaignLayer extends GameLayer {
     initiate() {
         if (this.isPlayerVictory) {
             this.victoryStreak += 1;
-            this.cpuPlayerNumber = this.victoryStreak%2==0? this.cpuPlayerNumber + 1 : this.cpuPlayerNumber;
+            this.cpuPlayerNumber = this.victoryStreak%3==0? this.cpuPlayerNumber + 1 : this.cpuPlayerNumber;
         }
         super.initiate();
         this.victoryBanner = new StrokedText(this.victoryStreak == undefined ? "" : "VICTORY STREAK: ".concat(this.victoryStreak), canvasWidth*0.5, getCanvasProportionSize(6), true);
         this.victoryBanner.setSize(getCanvasProportionSize(18));
+        this.setRivalPlayerScores();
     }
 
     draw() {
@@ -28,6 +29,13 @@ class CampaignLayer extends GameLayer {
     playVictory() {
         super.playVictory();
         this.isPlayerVictory = this.players[0].isVictorious;
+    }
+
+    setRivalPlayerScores() {
+        this.players.forEach(p => p.score = Math.floor(
+                Math.pow(this.victoryStreak/30, 2)/Math.pow(this.victoryStreak/30, 3)*30 + Math.random()*50 + 150
+            )
+        )
     }
 }
 
